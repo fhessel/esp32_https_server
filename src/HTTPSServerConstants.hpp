f41c4a05707b0aed61acde677034f1ae67df35ea
Frank Hessel <frank@fhessel.de>
2019-08-21 18:59:47 +0200

    Allow modification of HTTPSServerConstants using CFLAGS (#49)

--- src/HTTPSServerConstants.hpp
+++ src/HTTPSServerConstants.hpp
@@ -45,29 +45,45 @@
 // The following lines define limits of the protocol. Exceeding these limits will lead to a 500 error
 
 // Maximum of header lines that are parsed
+#ifndef HTTPS_REQUEST_MAX_HEADERS
 #define HTTPS_REQUEST_MAX_HEADERS               20
+#endif
 
 // Maximum length of the request line (GET /... HTTP/1.1)
+#ifndef HTTPS_REQUEST_MAX_REQUEST_LENGTH
 #define HTTPS_REQUEST_MAX_REQUEST_LENGTH       128
+#endif
 
 // Maximum length of a header line (including name and value)
+#ifndef HTTPS_REQUEST_MAX_HEADER_LENGTH
 #define HTTPS_REQUEST_MAX_HEADER_LENGTH        384
+#endif
 
 // Chunk size used for reading data from the ssl-enabled socket
+#ifndef HTTPS_CONNECTION_DATA_CHUNK_SIZE
 #define HTTPS_CONNECTION_DATA_CHUNK_SIZE       512
+#endif
 
 // Size (in bytes) of the Connection:keep-alive Cache (we need to be able to
 // store-and-forward the response to calculate the content-size)
+#ifndef HTTPS_KEEPALIVE_CACHESIZE
 #define HTTPS_KEEPALIVE_CACHESIZE              1400
+#endif
 
 // Timeout for an HTTPS connection without any transmission
+#ifndef HTTPS_CONNECTION_TIMEOUT
 #define HTTPS_CONNECTION_TIMEOUT               20000
+#endif
 
 // Timeout used to wait for shutdown of SSL connection (ms)
 // (time for the client to return notify close flag) - without it, truncation attacks might be possible
+#ifndef HTTPS_SHUTDOWN_TIMEOUT
 #define HTTPS_SHUTDOWN_TIMEOUT                 5000
+#endif
 
 // Length of a SHA1 hash
+#ifndef HTTPS_SHA1_LENGTH
 #define HTTPS_SHA1_LENGTH                      20
+#endif
 
 #endif /* SRC_HTTPSSERVERCONSTANTS_HPP_ */
